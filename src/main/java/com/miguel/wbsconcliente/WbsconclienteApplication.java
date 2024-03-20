2dd81411206dd55199c3ca71987b43dbe655b5fb
Miguel Mier <miguel.mier@babelgroup.com>
2024-03-20 10:51:24 +0100

    commit inicial

---
+++ src/main/java/com/miguel/wbsconcliente/WbsconclienteApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.wbsconcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbsconclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbsconclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/wbsconcliente/WbsconclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.wbsconcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbsconclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
